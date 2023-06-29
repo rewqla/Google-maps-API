02b856dd5c690b31edde7e58f7b8d95a6ed60436
rewqla <desares526@gmail.com>
2023-06-29 21:18:32 +0300

    Added a coordinates geocoder and improved previous code

--- script.js
+++ script.js
@@ -27,9 +27,33 @@ function geocodeAddress() {
             const resultText = `Address: ${formattedAddress}\nCoordinates: ${lat}, ${lng}`;
 
             resultContainer.innerText = resultText;
-            action.innerText = "Geocoding address";
         })
-        .catch((e) => {
-            alert("Geocode was not successful for the following reason: " + e);
-        });
+        .catch((error) => resultContainer.innerText = "The address geocoder failed due to: " + error)
+        .finally(() => action.innerText = "Geocoding coordinates");
+    ;
+}
+
+function geocodeCoordinates() {
+    const coordinates = document.getElementById("coordinates").value;
+    const latlngStr = coordinates.split(",", 2);
+
+    const location = {
+        lat: parseFloat(latlngStr[0]),
+        lng: parseFloat(latlngStr[1]),
+    }
+
+    geocoder.geocode({ location: location })
+        .then((response) => {
+            const { results } = response;
+            if (results[0]) {
+                const formattedAddress = results[0].formatted_address;
+                const resultText = `Address: ${formattedAddress}\nCoordinates: ${location.lat}, ${location.lng}`;
+
+                resultContainer.innerText = resultText;
+            } else {
+                resultContainer.innerText = "No results found";
+            }
+        })
+        .catch((error) => resultContainer.innerText = "The coordinates geocoder failed due to: " + error)
+        .finally(() => action.innerText = "Geocoding coordinates");
 }
