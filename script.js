7a32ffb5fa5f465c94055f7b2ccd3309d51a9255
rewqla <desares526@gmail.com>
2023-06-29 19:05:16 +0300

    Added geocoding functionality to convert addresses into coordinates

--- script.js
+++ script.js
@@ -1,9 +1,35 @@
-
+let map, geocoder, resultContainer, action;
 function initializeMap() {
     let mapProperties = {
         zoom: 8,
         center: { lat: 50.625898, lng: 26.200207 }
     }
 
-    let map = new google.maps.Map(document.getElementById("map_canvas"), mapProperties);
+    map = new google.maps.Map(document.getElementById("map_canvas"), mapProperties);
+    geocoder = new google.maps.Geocoder();
+    resultContainer = document.getElementById("result_container");
+    action = document.getElementById("action");
+}
+
+function geocodeAddress() {
+    const address = document.getElementById("address").value;
+
+    const request = {
+        address: address
+    };
+
+    geocoder.geocode(request)
+        .then((result) => {
+            const { results } = result;
+            const formattedAddress = results[0].formatted_address;
+            const lat = results[0].geometry.location.lat();
+            const lng = results[0].geometry.location.lng();
+            const resultText = `Address: ${formattedAddress}\nCoordinates: ${lat}, ${lng}`;
+
+            resultContainer.innerText = resultText;
+            action.innerText = "Geocoding address";
+        })
+        .catch((e) => {
+            alert("Geocode was not successful for the following reason: " + e);
+        });
 }
