c166c1dbe1da1d5281cdf40152fcc3060ffc5ead
rewqla <desares526@gmail.com>
2023-06-30 20:10:44 +0300

    Added auto complete with markers

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 let map, geocoder, resultContainer, action;
 function initializeMap() {
+    console.log(navigator.language)
     let mapProperties = {
         zoom: 8,
         center: { lat: 50.625898, lng: 26.200207 }
@@ -9,6 +10,8 @@ function initializeMap() {
     geocoder = new google.maps.Geocoder();
     resultContainer = document.getElementById("result_container");
     action = document.getElementById("action");
+
+    autocompleteAddress();
 }
 
 function geocodeAddress() {
@@ -56,4 +59,57 @@ function geocodeCoordinates() {
         })
         .catch((error) => resultContainer.innerText = "The coordinates geocoder failed due to: " + error)
         .finally(() => action.innerText = "Geocoding coordinates");
+}
+
+//add &libraries=places to google script
+function autocompleteAddress() {
+    const autoAddress = document.getElementById("auto-address");
+    const options = {
+        fields: ["formatted_address", "geometry", "name"],
+        strictBounds: false,
+        types: ["establishment"],
+    };
+
+    const autocomplete = new google.maps.places.Autocomplete(autoAddress, options);
+    autocomplete.bindTo("bounds", map);
+
+    const infowindow = new google.maps.InfoWindow();
+    const infowindowContent = document.getElementById("infowindow-content");
+
+    infowindow.setContent(infowindowContent);
+
+    const marker = new google.maps.Marker({
+        map,
+        anchorPoint: new google.maps.Point(0, -29),
+    });
+
+    autocomplete.addListener("place_changed", () => {
+        console.log("changed")
+        infowindow.close();
+        marker.setVisible(false);
+
+        const place = autocomplete.getPlace();
+
+        if (!place.geometry || !place.geometry.location) {
+            window.alert(
+                "No details available for input: '" + place.name + "'"
+            );
+            return;
+        }
+
+        // If the place has a geometry, then present it on a map.
+        if (place.geometry.viewport) {
+            map.fitBounds(place.geometry.viewport);
+        } else {
+            map.setCenter(place.geometry.location);
+            map.setZoom(17);
+        }
+
+        marker.setPosition(place.geometry.location);
+        marker.setVisible(true);
+        infowindowContent.children["place-name"].textContent = place.name;
+        infowindowContent.children["place-address"].textContent =
+            place.formatted_address;
+        infowindow.open(map, marker);
+    })
 }
